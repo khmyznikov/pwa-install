6d7b3994f1d603f0fa6c1cde94be5879f6914566
Khmyznikov Gleb <khmyznikov@gmail.com>
2020-11-08 19:11:06 +0300

    Rename component

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,8 @@ import { customElement, LitElement, html } from 'lit-element';
 import styles from './styles.scss';
 import template from './template';
 
-@customElement('root-el')
-export class RootElement extends LitElement {
+@customElement('pwa-install')
+export class PWAInstallElement extends LitElement {
 
   static get styles() {
     return styles;
