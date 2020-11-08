ba44158d724d19e19f32270a3b195668b85f08de
Khmyznikov Gleb <khmyznikov@gmail.com>
2020-11-08 21:58:54 +0300

    add styles and template, prepare for logic rework

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,161 @@
-import { customElement, LitElement, html } from 'lit-element';
+import { customElement, LitElement, html, property } from 'lit-element';
+
+import { IBeforeInstallPromptEvent, IRelatedApp, IChoiceResult, IManifest, Manifest, IWindow } from './types/pwa-install.types';
+
+declare const window: IWindow;
+
+declare global {
+	interface WindowEventMap {
+		beforeinstallprompt: IBeforeInstallPromptEvent
+	}
+}
 
 import styles from './styles.scss';
 import template from './template';
 
 @customElement('pwa-install')
 export class PWAInstallElement extends LitElement {
+	private manifest: IManifest = new Manifest();
 
-  static get styles() {
-    return styles;
-  }
+	@property()
+	icon = '';
 
-  render() {
-    return html`${template('Hello', 'World')}`;
-  }
+	@property()
+	name = '';
+
+	@property()
+	description = '';
+
+	static get styles() {
+		return styles;
+	}
+
+	private platforms = '';
+	private userChoiceResult = '';
+	private hideInstall: boolean = JSON.parse(window.sessionStorage.getItem('pwa-hide-install') || 'false');
+	private installAvailable = false;
+	private appleInstallAvailable = false;
+	private applePlatform = false;
+	private howToRequested = false;
+	private isUnderStandaloneMode = false;
+	private isRelatedAppsInstalled = false;
+
+	public install() {
+		if (window.deferredEvent) {
+			window.deferredEvent.prompt();
+			window.deferredEvent.userChoice
+				.then((choiceResult: IChoiceResult) => {
+					this.userChoiceResult = choiceResult.outcome;
+					/**
+					 * @event pwa-install-install
+					 */
+					console.log(choiceResult);
+				})
+				.catch((error) => {
+					/**
+					 * @event pwa-install-error
+					 */
+				});
+			window.deferredEvent = null;
+			this.installAvailable = false;
+		}
+	}
+
+	public hide() {
+		this.installAvailable = false;
+		this.appleInstallAvailable = false;
+		this.hideInstall = true;
+		window.sessionStorage.setItem('pwa-hide-install', 'true');
+	}
+
+	public howTo() {
+		this.howToRequested = !this.howToRequested;
+	}
+
+	private isAppleMobile() {
+		if (
+			(
+				['iPhone', 'iPad', 'iPod'].includes(navigator.platform) ||
+				(navigator.userAgent.match(/Mac/) && navigator.maxTouchPoints && navigator.maxTouchPoints > 2)
+			)
+			&& ('serviceWorker' in navigator))
+			return true;
+		return false;
+	}
+	private isStandalone() {
+		if (window.matchMedia('(display-mode: standalone)').matches || ('standalone' in navigator && (navigator as any).standalone === true))
+			return true;
+		return false;
+	}
+	private checkInstalled() {
+		this.isUnderStandaloneMode = this.isStandalone();
+
+		if ('getInstalledRelatedApps' in navigator)
+			(navigator as any).getInstalledRelatedApps().then((relatedApps: IRelatedApp[]) => {
+				this.isRelatedAppsInstalled = relatedApps.find((app: IRelatedApp) => app.id === 'com.hostmeapp.hostmepanel') ? true : false;
+			});
+
+		if (this.isAppleMobile()) {
+			this.applePlatform = true;
+			if (!this.hideInstall && !this.isUnderStandaloneMode)
+				setTimeout(
+					() => {
+						this.appleInstallAvailable = true;
+					},
+					300
+				);
+		}
+	}
+
+	private created = () => {
+		window.deferredEvent = null;
+
+		this.checkInstalled();
+
+		window.addEventListener('beforeinstallprompt', (e: IBeforeInstallPromptEvent) => {
+			window.deferredEvent = e;
+			e.preventDefault();
+
+			this.platforms = e.platforms;
+
+			if (this.userChoiceResult === 'dismissed' || this.userChoiceResult === 'accepted' || this.hideInstall || this.isRelatedAppsInstalled || this.isUnderStandaloneMode) {
+				this.installAvailable = false;
+			} else {
+				this.installAvailable = true;
+			}
+
+		});
+
+		window.addEventListener('appinstalled', (e) => {
+			window.deferredEvent = null;
+			this.installAvailable = false;
+		});
+
+		if (!this.name || !this.icon) {
+			fetch('/manifest.json').then((response: Response) => {
+				if (response.ok)
+					response.json().then((_json) => {
+						this.icon = _json.icons[0].src;
+						this.name = _json['short_name'] || _json.name;
+						this.description = _json.description;
+
+						this.manifest = _json;
+					});
+				else {
+					this.icon = this.manifest.icons[0].src;
+					this.name = this.manifest['short_name'];
+					this.description = this.manifest.description;
+				}
+			});
+		}
+	};
+
+	connectedCallback() {
+		this.created();
+		super.connectedCallback();
+	}
+
+	render() {
+		return html`${template(this.name, this.description)}`;
+	}
 }
---
+++ src/types/pwa-install.types.ts
@@ -0,0 +1,44 @@
+export interface IChoiceResult {
+    outcome: 'accepted' | 'dismissed';
+    platform: string;
+}
+
+export interface IBeforeInstallPromptEvent extends Event {
+    platforms: string;
+    userChoice: Promise<IChoiceResult>;
+    prompt: () => {};
+}
+
+export interface IRelatedApp {
+    id: string,
+    platform: string,
+    url: string
+}
+
+export interface IWindow extends Window {
+    deferredEvent: IBeforeInstallPromptEvent | null;
+}
+
+export interface IPWAIcon {
+    src: string;
+}
+
+export interface IManifest {
+    short_name: string;
+    name: string;
+    description: string;
+    icons: IPWAIcon[];
+}
+
+export class Manifest {
+    constructor() {
+        this.icons = [{ src: '' }];
+        this.short_name = 'PWA';
+        this.name = 'Progressive web application';
+        this.description = 'Progressive web application';
+    }
+    short_name: string;
+    icons: IPWAIcon[];
+    name: string;
+    description: string;
+}
