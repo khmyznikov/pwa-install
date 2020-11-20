a3bf16326954a89303514f558d6ca6f4d588d2de
Gleb Khmyznikov <gleb.khmyznikov@gmail.com>
2020-11-20 21:13:39 +0300

    Add terser plugin

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,39 +1,59 @@
+const TerserPlugin = require('terser-webpack-plugin');
+
 const { resolve } = require('path');
 
 module.exports = {
-  resolve: {
-    extensions: ['.ts', '.js']
-  },
-  devtool: 'source-map',
-  module: {
-    rules: [
-      {
-        test: /\.ts?$/,
-        loader: 'ts-loader',
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.scss$/,
-        use: [{
-          loader: 'lit-scss-loader',
-          options: {
-            minify: true,
-          },
-        }, 'extract-loader', 'css-loader', 'sass-loader'],
-      },
-    ]
-  },
-  plugins: [
-
-  ],
-  output: {
-    filename: 'bundle.js',
-    path: resolve(__dirname, '../dist'),
-  },
-  devServer: {
-    contentBase: resolve(__dirname, '../public'),
-    compress: false,
-    host: '0.0.0.0',
-    port: 3000
-  }
+	resolve: {
+		extensions: ['.ts', '.js']
+	},
+	devtool: 'source-map',
+	module: {
+		rules: [{
+				test: /\.ts?$/,
+				loader: 'ts-loader',
+				exclude: /node_modules/,
+			},
+			{
+				test: /\.scss$/,
+				use: [{
+					loader: 'lit-scss-loader',
+					options: {
+						minify: true,
+					},
+				}, 'extract-loader', 'css-loader', 'sass-loader'],
+			},
+		]
+	},
+	plugins: [],
+	optimization: {
+        minimize: true,
+        minimizer: [
+            new TerserPlugin({
+                test: /\.js$/,
+                extractComments: false,
+                terserOptions: {
+                    compress: {
+                        drop_console: true,
+                        unsafe: true
+                    },
+                    mangle: true,
+                    output: {
+                        comments: false,
+                        beautify: false,
+                    },
+                    safari10: true,
+                },
+            })
+        ],
+    },
+	output: {
+		filename: 'bundle.js',
+		path: resolve(__dirname, '../dist'),
+	},
+	devServer: {
+		contentBase: resolve(__dirname, '../public'),
+		compress: false,
+		host: '0.0.0.0',
+		port: 3000
+	}
 };
