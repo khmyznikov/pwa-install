9104901c3078d0dbf5cb885a18be4e59799ff1fa
khmyznikov <gleb.khmyznikov@gmail.com>
2022-07-23 20:16:49 +0300

    Revert turkish translation, fix deutch

--- src/localization/locales/de.ts
+++ src/localization/locales/de.ts
@@ -9,15 +9,15 @@
     /* eslint-disable @typescript-eslint/no-explicit-any */
 
     export const templates = {
-      's6196153c4b0c1ea0': `Install`,
-'sdca23702207fba73': `This site has app functionality. Install it on your device for extensive experience and easy access.`,
-'sa5ef80b4bb9b39f8': `Less`,
-'s37a9e8aec5713460': `More`,
-'s8d0173763d91830c': `This site has app functionality. Add it to your Home Screen for extensive experience and easy access.`,
-'sdfe23506a3b0cdc8': `1) Open in Safari browser`,
-'s1b9047d53d9f9d22': `2) Press Share in Navigation bar`,
-'s5c6aad7a7e4a1437': `3) Press Add to Home Screen`,
-'sc79fd7641eb9a975': `Hide Instruction`,
-'scdaf4bbff76674c8': `Add to Home Screen`,
+      's1b9047d53d9f9d22': `2) Drücken Sie auf Teilen in der Navigationsleiste`,
+'s37a9e8aec5713460': `Mehr`,
+'s5c6aad7a7e4a1437': `3) Drücken Sie Hinzufügen zum Startbildschirm`,
+'s6196153c4b0c1ea0': `Installieren`,
+'s8d0173763d91830c': `Diese Website verfügt über eine App-Funktionalität. Sie können die App auf Ihrem Gerät installieren, um eine bessere Erfahrung und einen einfachen Zugang zu genießen.`,
+'sa5ef80b4bb9b39f8': `Weniger`,
+'sc79fd7641eb9a975': `Anleitung ausblenden`,
+'scdaf4bbff76674c8': `Hinzufügen zum Startbildschirm`,
+'sdca23702207fba73': `Diese Website verfügt über eine App-Funktionalität. Sie können die App auf Ihrem Gerät installieren, um eine bessere Erfahrung und einen einfachen Zugang zu genießen.`,
+'sdfe23506a3b0cdc8': `1) In Safari-Browser öffnen`,
     };
   
--- src/localization/locales/tr.ts
+++ src/localization/locales/tr.ts
@@ -9,15 +9,15 @@
     /* eslint-disable @typescript-eslint/no-explicit-any */
 
     export const templates = {
-      's1b9047d53d9f9d22': `2) Drücken Sie auf Teilen in der Navigationsleiste`,
-'s37a9e8aec5713460': `Mehr`,
-'s5c6aad7a7e4a1437': `3) Drücken Sie Hinzufügen zum Startbildschirm`,
-'s6196153c4b0c1ea0': `Installieren`,
-'s8d0173763d91830c': `Diese Website verfügt über eine App-Funktionalität. Sie können die App auf Ihrem Gerät installieren, um eine bessere Erfahrung und einen einfachen Zugang zu genießen.`,
-'sa5ef80b4bb9b39f8': `Weniger`,
-'sc79fd7641eb9a975': `Anleitung ausblenden`,
-'scdaf4bbff76674c8': `Hinzufügen zum Startbildschirm`,
-'sdca23702207fba73': `Diese Website verfügt über eine App-Funktionalität. Sie können die App auf Ihrem Gerät installieren, um eine bessere Erfahrung und einen einfachen Zugang zu genießen.`,
-'sdfe23506a3b0cdc8': `1) In Safari-Browser öffnen`,
+      's1b9047d53d9f9d22': `2) Gezinme çubuğunda Paylaş'a basın`,
+'s37a9e8aec5713460': `Daha Fazla`,
+'s5c6aad7a7e4a1437': `3) Ana Ekrana Ekle'ye basın`,
+'s6196153c4b0c1ea0': `Yükle`,
+'s8d0173763d91830c': `Bu sitenin uygulaması var. Daha iyi deneyim ve kolay erişim için Ana Ekranınıza ekleyin.`,
+'sa5ef80b4bb9b39f8': `Küçült`,
+'sc79fd7641eb9a975': `Talimatları gizle`,
+'scdaf4bbff76674c8': `Ana Ekrana Ekle`,
+'sdca23702207fba73': `Bu sitenin uygulaması var. Daha iyi deneyim ve kolay erişim için cihazınıza yükleyebilirsiniz.`,
+'sdfe23506a3b0cdc8': `1) Safari tarayıcısında açın`,
     };
   
