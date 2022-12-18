497d352feeabc9aa6c65a6f93133804b83321758
khmyznikov <gleb.khmyznikov@gmail.com>
2022-12-18 22:22:14 +0100

    update packages, make new builds

---
+++ webpack/serve.js
@@ -0,0 +1,16 @@
+import express  from 'express';
+import { join, dirname } from 'path';
+import {fileURLToPath} from 'url';
+
+const app = express();
+const port = 3000;
+
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+app.use(express.static(join(__dirname, '../public')));
+app.use(express.static('./dist'));
+
+app.listen(port, () => console.log(`App running on ${port}.`));
