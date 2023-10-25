f02dc34299179b2a11431103d55d2d5eb64f6e04
Gleb Khmyznikov <gleb.khmyznikov@gmail.com>
2023-10-25 18:01:58 +0200

    Move public to docs for gh pages

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -1,40 +1,59 @@
-import common from './webpack.common.js';
-import webpack from 'webpack';
-import { merge }  from 'webpack-merge';
+import common from "./webpack.common.js";
+import webpack from "webpack";
+import { merge } from "webpack-merge";
 
-import TerserPlugin from 'terser-webpack-plugin';
-import { CleanWebpackPlugin } from 'clean-webpack-plugin';
+import path, { resolve } from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+import TerserPlugin from "terser-webpack-plugin";
+import { CleanWebpackPlugin } from "clean-webpack-plugin";
+import FileManagerPlugin from "filemanager-webpack-plugin";
 
 export default merge(common, {
-    output: {
-        filename: 'pwa-install.bundle.js',
-        library: 'PWAInstallComponent'
-    },
-	plugins: [
-        new CleanWebpackPlugin(),   
-        new webpack.optimize.LimitChunkCountPlugin({
-            maxChunks: 1
-        })
+  output: {
+    filename: "pwa-install.bundle.js",
+    library: "PWAInstallComponent",
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new webpack.optimize.LimitChunkCountPlugin({
+      maxChunks: 1,
+    }),
+    new FileManagerPlugin({
+      events: {
+        onEnd: {
+          copy: [
+            {
+              source: resolve(__dirname, "../dist/pwa-install.bundle.js"),
+              destination: resolve(__dirname, "../docs/pwa-install.bundle.js"),
+            },
+          ],
+        },
+      },
+    }),
+  ],
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        test: /\.js$/,
+        extractComments: false,
+        terserOptions: {
+          compress: {
+            drop_console: true,
+            unsafe: true,
+          },
+          mangle: true,
+          output: {
+            comments: false,
+            beautify: false,
+          },
+        },
+      }),
     ],
-	optimization: {
-        minimize: true,
-        minimizer: [
-            new TerserPlugin({
-                test: /\.js$/,
-                extractComments: false,
-                terserOptions: {
-                    compress: {
-                        drop_console: true,
-                        unsafe: true
-                    },
-                    mangle: true,
-                    output: {
-                        comments: false,
-                        beautify: false,
-                    }
-                },
-            })
-        ],
-    },
-	devtool: 'source-map'
+  },
+  devtool: "source-map",
 });
