cf83e1ba5baed96a2c58bd4db35d1e33cfa30417
Julian Lamprea <jalamprea@gmail.com>
2024-03-08 12:44:54 -0500

    Support disable-cose also on desktop view

--- src/templates/chrome/template.ts
+++ src/templates/chrome/template.ts
@@ -17,9 +17,9 @@ const template = (name: string, description: string, installDescription: string,
                     <div class="about">
                         <div class="name">
                             <label>${name}</label>
-                            <button class="material-button secondary close" @click='${hideDialog}'>
+                            ${!disableClose? html`<button class="material-button secondary close" @click='${hideDialog}'>
                                 <svg viewBox="0 0 24 24"><path d="M5.3 18.7c.2.2.4.3.7.3s.5-.1.7-.3l5.3-5.3 5.3 5.3a1.08 1.08 0 0 0 .7.3 1.08 1.08 0 0 0 .7-.3c.4-.4.4-1 0-1.4L13.4 12l5.3-5.3c.4-.4.4-1 0-1.4s-1-.4-1.4 0L12 10.6 6.7 5.3c-.4-.4-1-.4-1.4 0s-.4 1 0 1.4l5.3 5.3-5.3 5.3c-.4.4-.4 1 0 1.4z"/></svg>
-                            </button>
+                            </button>` : ''}
                         </div>
                         <div class="hostname">${location.hostname}</div>
                         
