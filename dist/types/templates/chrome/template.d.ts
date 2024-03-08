abf1852f364a8d46018a4872e0cdf4d040884b2d
Julian Lamprea <jalamprea@gmail.com>
2024-03-08 12:28:57 -0500

    Added build output

--- dist/types/templates/chrome/template.d.ts
+++ dist/types/templates/chrome/template.d.ts
@@ -1,3 +1,3 @@
 import { WebAppManifest } from 'web-app-manifest';
-declare const template: (name: string, description: string, installDescription: string, disableDescription: boolean, disableScreenshots: boolean, icon: string, manifest: WebAppManifest, installAvailable: any, hideDialog: any, install: any, toggleGallery: any, galleryRequested: boolean) => import("lit-html").TemplateResult<1>;
+declare const template: (name: string, description: string, installDescription: string, disableDescription: boolean, disableScreenshots: boolean, disableClose: boolean, icon: string, manifest: WebAppManifest, installAvailable: any, hideDialog: any, install: any, toggleGallery: any, galleryRequested: boolean) => import("lit-html").TemplateResult<1>;
 export default template;
