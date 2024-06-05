30250ead90c5d555ba7b716f26d2e2354e429163
parsaM110 <pmomeni80@gmail.com>
2024-06-05 10:09:00 +0330

    adding fa-IR

---
+++ src/localization/locales/fa-IR.ts
@@ -0,0 +1,27 @@
+
+    // Do not modify this file by hand!
+    // Re-generate this file by running lit-localize
+
+    
+    
+
+    /* eslint-disable no-irregular-whitespace */
+    /* eslint-disable @typescript-eslint/no-explicit-any */
+
+    export const templates = {
+      's182ab2d6c997515f': `برای تجربه گسترده و دسترسی آسان آن را به صفحه اصلی خود اضافه کنید.`,
+'s1b9047d53d9f9d22': `۲) در نوار پایین دکمه (Share) را انتخاب کنید`,
+'s37a9e8aec5713460': `بیشتر`,
+'s5c6aad7a7e4a1437': `۳) را انتخاب کنید (Add to Home Screen)`,
+'s6196153c4b0c1ea0': `نصب`,
+'s922329d6f6213590': `افزودن به (Dock)`,
+'sa5ef80b4bb9b39f8': `کمتر`,
+'sa7551383d1897fcc': `۲) را انتخاب کنید (Add to Dock)`,
+'sba52286c21552a4e': `برای تجربه گسترده و دسترسی آسان آن را بر روی دستگاه خود نصب کنید.`,
+'sc79fd7641eb9a975': ` مخفی کردن مراحل`,
+'scdaf4bbff76674c8': `افزودن به (Home Screen)`,
+'sdfe23506a3b0cdc8': `۱)  مرورگر  (Safari) را باز کنید  `,
+'se740f75b95a51807': `برای تجربه گسترده و دسترسی آسان آن را به Dock خود اضافه کنید.`,
+'sfea652f6580ff086': ` این سایت دارای قابلیت اپلیکیشن است. `,
+    };
+  
