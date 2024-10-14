5ca2b65c942fdcd24f5fa40d28428aed6215dad1
Henrik Cullen <hccullen@users.noreply.github.com>
2024-10-14 17:18:22 +0200

    Feat: Filter screenshots based on form_factor

--- src/gallery/index.ts
+++ src/gallery/index.ts
@@ -1,13 +1,13 @@
 import { LitElement, html } from 'lit';
 import { property, customElement } from 'lit/decorators.js';
-import { WebAppManifest } from 'web-app-manifest';
+import { PWAScreenhot } from '../types/types';
 
 import styles from './styles-gallery.scss';
 import template from './template-gallery';
 
 @customElement('pwa-gallery')
 export default class PWAGalleryElement extends LitElement {
-	@property() screenshots: WebAppManifest['screenshots'] = [];
+	@property() screenshots: PWAScreenhot[] = [];
 	@property() theme: 'default' | 'apple_desktop' | 'apple_mobile' = 'default';
 
 	static get styles() {
