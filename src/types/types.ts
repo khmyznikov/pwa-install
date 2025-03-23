760bb1b312cca53c933367222c498396bdcbe2d9
Gleb Khmyznikov <gleb.khmyznikov@gmail.com>
2025-03-23 12:29:57 +0100

    bump sources, fix android fallback style

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,4 +1,4 @@
-import { ImageResource } from 'web-app-manifest';
+import { ImageResource, WebAppManifest } from 'web-app-manifest';
 
 export interface IRelatedApp {
     id: string,
@@ -27,11 +27,11 @@ export interface PWAInstallAttributes {
 
 export interface ManifestScreenshot extends ImageResource {
     label?: string;
-    platform?: string;
+    platform?: "android" | "chromeos" | "ipados" | "ios" | "kaios" | "macos" | "windows" | "xbox" | "chrome_web_store" | "play" | "itunes" | "microsoft-inbox" | "microsoft-store";
     form_factor?: "wide" | "narrow";
 }
 
-export class Manifest {
+export class Manifest implements WebAppManifest {
     constructor() {
         this.icons = [{ src: '' }];
         this.screenshots = undefined;
