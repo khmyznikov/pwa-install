2bd8275db09df281f4ffb2c802d16faa0a7e244e
Gleb Khmyznikov <gleb.khmyznikov@gmail.com>
2025-12-07 18:49:26 -0800

    update translation

--- dist/types/localization/locales/sr.d.ts
+++ dist/types/localization/locales/sr.d.ts
@@ -1,16 +1,18 @@
 export declare const templates: {
     s1488e73779d7682f: string;
     s182ab2d6c997515f: string;
-    s1b9047d53d9f9d22: string;
-    s2d90afa5dcca2c0e: string;
     s37a9e8aec5713460: string;
+    s3e31cb694b5e4f47: string;
     s4593f304dac8848c: string;
-    s54cfbb14f46396d4: string;
     s6196153c4b0c1ea0: string;
+    s681e399d63311fba: string;
+    s9c30494cee3b81dd: string;
+    s9d0493c79a1c5f47: string;
     sa5ef80b4bb9b39f8: string;
     sa7551383d1897fcc: string;
     sba52286c21552a4e: string;
-    sc79fd7641eb9a975: string;
+    sc16e00a7a8b2fde2: string;
+    sdb168a692f9130e1: string;
     se740f75b95a51807: string;
     sfea652f6580ff086: string;
     s922329d6f6213590: string;
