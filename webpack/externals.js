5d4e5c83110dfc68cd6b40eb9ea01daed21ffd85
Gleb Khmyznikov <gleb.khmyznikov@gmail.com>
2025-12-13 21:35:23 -0800

    trying to solve safari problem

--- webpack/externals.js
+++ webpack/externals.js
@@ -2,7 +2,11 @@ export default {
 	lit: 'lit',
 	'lit/decorators.js': 'lit/decorators.js',
 	'lit/directives/class-map.js': 'lit/directives/class-map.js',
+	'lit/directives/style-map.js': 'lit/directives/style-map.js',
+	'@lit/reactive-element': '@lit/reactive-element',
+	'lit-html': 'lit-html',
+	'lit-element': 'lit-element',
 	'@khmyznikov/pwa-install': '@khmyznikov/pwa-install',
 	'react': 'react',
-	// '@lit/localize': '@lit/localize',
+	'@lit/localize': '@lit/localize',
 }
