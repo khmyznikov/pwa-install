ede6375f8901d1fbf8f8d436205282be3a1f46cd
Gleb Khmyznikov <gleb.khmyznikov@gmail.com>
2026-01-30 10:40:51 -0800

    new build

---
+++ dist/types/localization/locales/bg.d.ts
@@ -0,0 +1,21 @@
+export declare const templates: {
+    s182ab2d6c997515f: string;
+    s224cbcec014ef6b5: string;
+    s37a9e8aec5713460: string;
+    s386eca8362ff6155: string;
+    s4e1e10a6ca408245: string;
+    s6196153c4b0c1ea0: string;
+    s633502f7cff4f847: string;
+    s681e399d63311fba: string;
+    s7f0591f08e318eda: string;
+    s8114bd55cae5a22b: string;
+    s922329d6f6213590: string;
+    s9af56bf005b49c74: string;
+    sa5ef80b4bb9b39f8: string;
+    sba52286c21552a4e: string;
+    sc16e00a7a8b2fde2: string;
+    scdaf4bbff76674c8: string;
+    se0e473adfda8066c: string;
+    se740f75b95a51807: string;
+    sfea652f6580ff086: string;
+};
